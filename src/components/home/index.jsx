9882773cdf77517e0ddb9c3d4f3efd405afa4400
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-07-04 15:23:52 +0200

    update home

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 
 export default class index extends Component {
   render() {
-    return (
-      <div>
-        home
-      </div>
-    )
+    return (<h1>Welkom to class!</h1>)
   }
 }

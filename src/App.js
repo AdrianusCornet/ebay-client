85e59caebfbbc0c4dd6f90c0835368841af430b8
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-07-04 15:23:11 +0200

    remove test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { Route } from 'react-router'
 import './App.css';
 import Home from './components/home'
-import Test from './components/test'
 
 function App() {
   return (
     <div className="App">
-      <span>dit is :</span>
       <Route exact path='/' component={Home} />
-      <Route exact path='/test' component={Test} />
     </div>
   );
 }

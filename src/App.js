8819de6f430876e7b7718fdbdd375f21c99e14ca
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-07-04 15:31:47 +0200

    provide store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+// imports
+// -npm
+import React, { Component } from 'react';
 import { Route } from 'react-router'
-import './App.css';
+import { Provider } from 'react-redux'
+// -componets
 import Home from './components/home'
+// -store
+import store from './store'
 
-function App() {
-  return (
-    <div className="App">
-      <Route exact path='/' component={Home} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <Route exact path='/' component={Home} />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
